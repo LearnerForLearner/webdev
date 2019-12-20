a9ddca699bda6b19227a66a5e469d5dfbe603e61
13260770366@163.com <13260770366@163.com>
2019-12-20 20:53:57 +0800

    add more

---
+++ javalsj-vue/src/components/common/BlogHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        清风徐来 水波不兴
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'BlogHeader'
+}
+</script>
---
+++ javalsj-vue/src/components/home/BlogIndex.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <blog-header></blog-header>
+    <hr/>
+    <div>
+      这是首页，嘻嘻嘻。
+    </div>
+    <hr/>
+    <blog-footer></blog-footer>
+  </div>
+</template>
+
+<script>
+import blogHeader from '@/components/common/BlogHeader.vue'
+import blogFooter from '@/components/common/BlogFooter.vue'
+
+export default {
+  name: 'BlogIndex',
+  // blogHeader/blogFooter组件给申明到components里面然后在template里面使用
+  components: { blogHeader, blogFooter }
+}
+</script>
+支持跨域，请求路由，页面路由开发
+main.js 主入口代码：
+
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 引用axios，并设置基础URL为后端服务api地址
+var axios = require('axios')
+axios.defaults.baseURL = 'https://localhost:8443/api'
+// 将API方法绑定到全局
+Vue.prototype.$axios = axios
+Vue.config.productionTip = false
+
+/* eslint-disable */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
