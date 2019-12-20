9c6b0475a506c90bb4629515cb8ef93d7d73ea38
LearnerForLearner <57471196+LearnerForLearner@users.noreply.github.com>
2019-12-20 21:19:20 +0800

    Update Result.java

--- Result.java
+++ Result.java
@@ -1,12 +1,6 @@
 package com.javalsj.blog.result;
 
-/**
- * @description 统一 API响应结果封装
- * @author WANGJIHONG
- * @date 2018年3月13日 下午8:44:29
- * @Copyright 版权所有 (c) www.javalsj.com
- * @memo 控制Result权限，构建结果Result对象统一使用com.javalsj.blog.vo.ResultFactory工厂类来创建
- */
+
 public class Result {
     /**
      * 响应状态码
